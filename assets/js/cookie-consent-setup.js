1807f9ed8066d109c47644cdd81b7be1f373ca67
linsome <108054222+linsome@users.noreply.github.com>
2026-05-05 23:28:22 +0000

    Deploying to gh-pages from @ linsome/jianlin.github.io@ed66f439671beecb0b5381484b3ac25c9d169896 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://linsome.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://linsome.github.io/jianlin.github.io/#contact">contact us</a>.'
               }
             ]
           }
